23a21910b26af79e4fd80c4ed12883f716e0b2b7
khen <30577427+khendrikse@users.noreply.github.com>
2022-08-17 17:46:39 +0200

    chore: update astro to use v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-export default {
-  buildOptions: {
-    sitemap: true,
-  },
-  renderers: ["@astrojs/renderer-preact"],
-};
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  integrations: [preact(), sitemap()],
+});
