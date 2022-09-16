ae0527c8eac5d3d5eff1341c814b84c3dbbffd43
aryagao <archegao@163.com>
2022-09-16 17:12:20 +0800

    add a redirect and function

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,8 @@
+exports.handler = async () => {
+    const mySecret = process.env.MY_SECRET;
+    return {
+      statusCode: 200,
+      body: `hello world! I have a ${mySecret}`,
+    };
+  };
+  
