80614de4fc61fa55ef66880f7f7252ebafed4059
Benson Arafat <benpaul320@gmail.com>
2023-08-15 04:11:43 +0100

    Update Revert

--- netlify/functions/hello-world.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => {
-    const mySecret = process.env.MY_SECRET;
-    return {
-      statusCode: 200,
-      body: `hello world! I have a ${mySecret}`,
-    };
-  };
-  
